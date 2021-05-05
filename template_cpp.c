35040b9798374c2f2fface7afb39a04c0ad1c8ff
Jayanth Nalluri <76648322+jayanth353@users.noreply.github.com>
2021-05-05 17:09:47 +0530

    Update template_cpp.c

--- template_cpp.c
+++ template_cpp.c
@@ -7,13 +7,10 @@ j   sum(j a,j b)
 {
   return(a+b);
 }
-template <class j>
-j sum(j a,j b,j c)
-{
-  return(a+b+c);
-}
+
+
 int main()
 {
-    cout << "sum is  !"<<sum(12.22,13.66);
-    cout << "sum is  !"<<sum(12.22,13.66,44.4);
+    cout << "sum 2+3="<<sum(2,3);
+    cout << "\nsum 1.1+2.2="<<sum(1.1,2.2);
 }
