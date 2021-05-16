197fa797eef6272e05d802f6ee4017f860fd0ca7
Jayanth Nalluri <76648322+jayanth353@users.noreply.github.com>
2021-05-16 21:05:46 +0530

    Update Addition_of_large_numbers.c

--- Addition_of_large_numbers.c
+++ Addition_of_large_numbers.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 #include<string.h>
-void cal(char*p1,char*p2,int min,int max)
+void cal(char*p1,char*p2,char *s1,char *s2,int min,int max)
 {
 	char res[1000],str2[1000];
 	int b;
@@ -26,6 +25,14 @@ void cal(char*p1,char*p2,int min,int max)
 		l++;
 	}
 	res[l]='\0';
+    printf(" %s",s2);
+    printf("\n+");
+    for(int k=0;k<max-min;k++)
+    printf(" ");
+    printf("%s\n ",s1);
+    for(int j=0;j<max;j++)
+    printf("-");
+    printf("\n ");
     for(int j=l-1;j>=0;j--)
     {
         printf("%c",res[j]);
@@ -38,35 +45,27 @@ int main(){
 	
 		int n1=0,n2=0,max=0,min=0,carry=0,i=0,r=0;
 		char s1[100],s2[100],str1[100],str2[100],res[100],a,b;
+        printf("Enter two numbers to add:\n");
 		scanf("%s %s",s1,s2);
+        //find length of the strings
 		for(n1=0;s1[n1]!='\0';n1++);
 		for(n2=0;s2[n2]!='\0';n2++);
-		if(n1>=n2)
-		{
-			max=n1;
-			min=n2;
-		}
-		else
-		{
-			max=n2;
-			min=n1;
-		}
-    
+		
+        //reversing s1
 	    for(i=n1-1,a=0;i>=0;i--,a++)
 		{
              str1[a]=s1[i];
 
 		}
 		str1[a]='\0';
+        //reversing s2
 		for(i=n2-1,b=0;i>=0;i--,b++)
 		{
              str2[b]=s2[i];
 
 		}
 		str2[b]='\0';
-       if(n1>=n2) cal(str2,str1,min,max);
-	   else cal(str1,str2,min,max);
+       if(n1>=n2) cal(str2,str1,s2,s1,n2,n1);
+	   else cal(str1,str2,s1,s2,n1,n2);
 	 
 }
-
-
